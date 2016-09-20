b7b76e061f95a2fc2c77f1e4d23e574fb35389b3
loisgh <loisgh@nyc.rr.com>
2016-09-20 12:52:24 -0400

    add routes to api

--- app.rb
+++ app.rb
@@ -7,3 +7,43 @@ require './environments'
 
 class MenuItem < ActiveRecord::Base
 end
+
+#view all items
+get "/" do
+  @menu_items = MenuItem.all
+  @menu_items.to_json
+end
+
+get "/category/:cat" do
+  @menu_items = MenuItem.where(m_type: params[:cat])
+  @menu_items.to_json
+end
+
+get "/categorylist" do
+  @categories = MenuItem.select('Distinct m_type').map(&:m_type)
+  @categories.to_json
+end
+
+get "/id/:id" do
+  @menu_items = MenuItem.find(params[:id])
+  @menu_items.to_json
+end
+
+get "/instructions" do
+
+end
+
+get "/byprice/:price" do
+  search_end = params[:price] += 2
+  @menu_items = MenuItemMenuItem.where(:price => params[:price]..search_end)
+  @menu_items.to_json
+end
+
+get "/bynamee/:name" do
+  @menu_items = MenuItem.where("name like ?", search)
+  @menu_items.to_json
+end
+
+
+
+
