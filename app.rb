b2076a6df06efc36e77ff902f75057234ac70e77
loisgh <loisgh@nyc.rr.com>
2016-09-20 15:35:25 -0400

    Add instruction page.  Also stylize list of categories

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ end
 
 get "/categorylist" do
   @categories = MenuItem.select('Distinct m_type').map(&:m_type)
-  @categories.to_json
+  erb :categories
 end
 
 get "/id/:id" do
@@ -29,21 +29,33 @@ get "/id/:id" do
   @menu_items.to_json
 end
 
-get "/instructions" do
-
-end
-
-get "/byprice/:price" do
-  search_end = params[:price] += 2
-  @menu_items = MenuItemMenuItem.where(:price => params[:price]..search_end)
+get "/price/:price" do
+  search_end = params[:price].to_f
+  search_end += 2
+  @menu_items = MenuItem.where(:price => params[:price].to_f..search_end)
   @menu_items.to_json
 end
 
-get "/bynamee/:name" do
+get "/name/:name" do
   @menu_items = MenuItem.where("name like ?", search)
   @menu_items.to_json
 end
 
+get "/instructions" do
+  send_file './assets/instructions.html'
+end
+
+get "/styles.css" do
+  send_file './assets/styles.css'
+end
+
+get "/Rabelo Regular.otf" do
+  send_file './assets/Rabelo Regular.otf'
+end
+
+get "/Rabelo Regular.ttf" do
+  send_file './assets/Rabelo Regular.ttf'
+end
 
 
 
